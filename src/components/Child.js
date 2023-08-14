decfa61b9fec561f554d66c6d1929e416631ad91
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-15 04:36:49 +0530

    sub

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,7 +3,6 @@ import React,{useState} from 'react'
 let Child= (props)=>{
     return(
         <div>
-        <h1>Child Component</h1>
         <button onClick={()=>{props.set(true)}}>Show Modal</button>
         {props.item && <div>
             <h1>Modal Content</h1>
